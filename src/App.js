dab0c76b376c27a20c6f74d809a7dba3f832679d
Franco Traverso <franco-t20@outlook.com>
2021-12-03 18:43:10 -0300

    add ItemListContainer component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "./components/layout/Navbar";
+import ItemListContainer from "./components/items/ItemListContainer";
 
 function App() {
   return (
     <>
       <Navbar />
+      <ItemListContainer name="Franco" />
     </>
   );
 }
