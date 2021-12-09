bc9b319bede940e61f3816a396120c8b44f63451
Franco Traverso <franco-t20@outlook.com>
2021-12-09 16:00:21 -0300

    render ItemList in ItemListContainer

--- src/components/items/ItemListContainer.js
+++ src/components/items/ItemListContainer.js
@@ -1,11 +1,12 @@
 import React from "react";
+import ItemList from "./ItemList";
 
 const ItemListContainer = (props) => {
   function onAdd(addedQuantity) {
     console.log(`added ${addedQuantity} items!`);
   }
 
-  return <div></div>;
+  return <ItemList />;
 };
 
 export default ItemListContainer;
