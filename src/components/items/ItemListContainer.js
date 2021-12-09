74f218747dc80049072bfe59938216bb756aad8c
Franco Traverso <franco-t20@outlook.com>
2021-12-08 23:03:52 -0300

    add ItemCount component

--- src/components/items/ItemListContainer.js
+++ src/components/items/ItemListContainer.js
@@ -1,9 +1,15 @@
 import React from "react";
+import ItemCount from "../layout/ItemCount";
 
 const ItemListContainer = (props) => {
+  function onAdd(addedQuantity) {
+    console.log(`added ${addedQuantity} items!`);
+  }
+
   return (
     <div>
       <h1>Hello {props.name}!</h1>
+      <ItemCount stock={5} initial={1} onAdd={onAdd} />
     </div>
   );
 };
---
+++ src/components/layout/ItemCount.js
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import { InputGroup, Button, Badge } from "react-bootstrap";
+
+const ItemCount = ({ stock, initial, onAdd }) => {
+  const [itemQuantity, setItemQuantity] = useState(initial);
+  const [availableStock, setAvailableStock] = useState(stock);
+
+  function onIncrease() {
+    if (itemQuantity < availableStock) {
+      setItemQuantity(itemQuantity + 1);
+    }
+  }
+
+  function onDecrease() {
+    if (itemQuantity > 0) {
+      setItemQuantity(itemQuantity - 1);
+    }
+  }
+
+  function onAddToCart() {
+    if (itemQuantity) {
+      onAdd(itemQuantity);
+      setAvailableStock(availableStock - itemQuantity);
+      setItemQuantity(0); //resets the counter
+    }
+  }
+
+  return (
+    <>
+      <InputGroup>
+        <Button onClick={onDecrease} variant="outline-secondary">
+          -
+        </Button>
+        <Button onClick={onIncrease} variant="outline-secondary">
+          +
+        </Button>
+        <Button onClick={onAddToCart} variant="dark">
+          Add to Cart <Badge bg="secondary">{itemQuantity}</Badge>
+        </Button>
+      </InputGroup>
+    </>
+  );
+};
+
+export default ItemCount;
