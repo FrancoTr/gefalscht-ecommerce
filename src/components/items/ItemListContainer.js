f3b12301eb65cc741babc46823e2276c9e78295f
Franco Traverso <franco-t20@outlook.com>
2021-12-09 12:26:27 -0300

    remove props example

--- src/components/items/ItemListContainer.js
+++ src/components/items/ItemListContainer.js
@@ -1,17 +1,11 @@
 import React from "react";
-import ItemCount from "../layout/ItemCount";
 
 const ItemListContainer = (props) => {
   function onAdd(addedQuantity) {
     console.log(`added ${addedQuantity} items!`);
   }
 
-  return (
-    <div>
-      <h1>Hello {props.name}!</h1>
-      <ItemCount stock={5} initial={1} onAdd={onAdd} />
-    </div>
-  );
+  return <div></div>;
 };
 
 export default ItemListContainer;
