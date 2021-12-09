452305e213b95488a8c4a89290a5e80b890cd3e6
Franco Traverso <franco-t20@outlook.com>
2021-12-09 15:59:01 -0300

    add Item component

--- src/components/items/Item.js
+++ src/components/items/Item.js
@@ -3,15 +3,15 @@ import { Card, ListGroup, ListGroupItem } from "react-bootstrap";
 
 const Item = ({ id, title, description, price, pictureUrl }) => {
   return (
-    <Card style={{ width: "18rem" }}>
+    <Card border="dark" bg="dark" text="light">
       <Card.Img variant="top" src={pictureUrl} />
       <Card.Body>
-        <Card.Title>{title}</Card.Title>
+        <Card.Title as="h1">{title}</Card.Title>
         <Card.Text>{description}</Card.Text>
       </Card.Body>
       <ListGroup className="list-group-flush">
-        <ListGroupItem>SKU: {id}</ListGroupItem>
-        <ListGroupItem>Price: ${price}</ListGroupItem>
+        <ListGroupItem variant="dark">SKU: {id}</ListGroupItem>
+        <ListGroupItem variant="dark">Price: {price}</ListGroupItem>
       </ListGroup>
     </Card>
   );
