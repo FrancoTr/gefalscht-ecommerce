65110afeb2a2e87e94543f5b647c1788f088220a
Franco Traverso <franco-t20@outlook.com>
2021-12-11 15:04:46 -0300

    add promiseMock function

--- src/components/items/ItemListContainer.js
+++ src/components/items/ItemListContainer.js
@@ -1,13 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ItemList from "./ItemList";
 import itemDataMock from "./ItemDataMock.json";
 
-const ItemListContainer = (props) => {
-  function onAdd(addedQuantity) {
-    console.log(`added ${addedQuantity} items!`);
-  }
+const promiseMock = () => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(itemDataMock);
+    }, 3000);
+  });
+};
 
-  return <ItemList items={itemDataMock} />;
+const ItemListContainer = (props) => {
+  const [itemsData, setItemsData] = useState([]);
+
+  useEffect(() => {
+    promiseMock().then((data) => {
+      setItemsData(data);
+    });
+  }, []);
+
+  /*function onAdd(addedQuantity) {
+    console.log(`added ${addedQuantity} items!`);
+  }*/
+
+  return <ItemList items={itemsData} />;
 };
 
 export default ItemListContainer;
