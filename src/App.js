b458fa8e6845e958e7a3f736e75bb24467b6d4e7
Franco Traverso <franco-t20@outlook.com>
2021-12-19 22:54:55 -0300

    Render ItemDetailComponent in the main section, above the rest of the products

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "./components/layout/Navbar";
 import ItemListContainer from "./components/items/ItemListContainer";
+import ItemDetailContainer from "./components/items/ItemDetailContainer";
 
 function App() {
   return (
     <>
       <Navbar />
+      <ItemDetailContainer />
       <ItemListContainer />
     </>
   );
