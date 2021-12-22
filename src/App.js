e161d96e76d7f043ef6a38a02117934d2d5669d9
Franco Traverso <franco-t20@outlook.com>
2021-12-21 22:41:03 -0300

    import react-router-dom elements

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "./components/layout/Navbar";
 import ItemListContainer from "./components/items/ItemListContainer";
 import ItemDetailContainer from "./components/items/ItemDetailContainer";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Navbar />
       <ItemDetailContainer />
       <ItemListContainer />
-    </>
+    </BrowserRouter>
   );
 }
 
