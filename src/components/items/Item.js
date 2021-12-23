e9d94ee28809bb65fad10a6bb4fa1cf812a15d07
Franco Traverso <franco-t20@outlook.com>
2021-12-23 18:01:59 -0300

    remove description field from Item component

--- src/components/items/Item.js
+++ src/components/items/Item.js
@@ -1,18 +1,20 @@
 import React from "react";
+import { LinkContainer } from "react-router-bootstrap";
 import { Card, Button } from "react-bootstrap";
 
-const Item = ({ title, description, image }) => {
+const Item = ({ id, title, image }) => {
   return (
     <Card border="dark" style={{ width: "18rem" }}>
       <Card.Img variant="top" src={image} />
       <Card.Body>
         <Card.Title className="fw-bold text-center">{title}</Card.Title>
-        <Card.Text>{description}</Card.Text>
-        <div className="text-center">
-          <Button size="lg" variant="dark">
-            See Details
-          </Button>
-        </div>
+        <LinkContainer to={`/item/${id}`}>
+          <div className="text-center">
+            <Button size="lg" variant="dark">
+              See Details
+            </Button>
+          </div>
+        </LinkContainer>
       </Card.Body>
     </Card>
   );
