7a0c09cd674db50813062eb37bb44431d27d221f
Franco Traverso <franco-t20@outlook.com>
2022-01-10 21:55:38 -0300

    add Cart functions to CartProvider

--- src/store/CartContext.js
+++ src/store/CartContext.js
@@ -1,11 +1,57 @@
-import React, { createContext } from "react";
+import React, { useContext, createContext, useState } from "react";
 
-const CartContext = createContext({
-  items: [],
-  addItem: (item, quantity) => {},
-  removeItem: (itemId) => {},
-  clear: () => {},
-  isInCart: (itemId) => {},
-});
+const cartContext = createContext();
+export const { Provider } = cartContext;
 
-export default CartContext;
+export const useCartContext = () => {
+  return useContext(cartContext);
+};
+
+const CartProvider = ({ children }) => {
+  const [totalQuantity, setTotalQuantity] = useState(0);
+  const [cart, setCart] = useState([]);
+
+  const addToCart = (product, quantity) => {
+    if (isInCart(product.id)) {
+      let productInCart = isInCart(product.id);
+      productInCart.quantity += quantity;
+      setCart([...cart]);
+    } else {
+      setCart([...cart, { product, quantity }]);
+    }
+    setTotalQuantity(totalQuantity + quantity);
+  };
+
+  const removeFromCart = (id) => {
+    const product = (id) => cart.find((product) => product.id === id);
+    setTotalQuantity(totalQuantity - product.quantity);
+
+    const removeProduct = cart.filter((product) => product.id !== id);
+    setCart(removeProduct);
+  };
+
+  const clearCart = () => {
+    setTotalQuantity(0);
+    setCart([]);
+  };
+
+  const isInCart = (id) => {
+    return cart.find((product) => product.id === id);
+  };
+
+  return (
+    <Provider
+      value={{
+        totalQuantity,
+        cart,
+        addToCart,
+        removeFromCart,
+        clearCart,
+      }}
+    >
+      {children}
+    </Provider>
+  );
+};
+
+export default CartProvider;
